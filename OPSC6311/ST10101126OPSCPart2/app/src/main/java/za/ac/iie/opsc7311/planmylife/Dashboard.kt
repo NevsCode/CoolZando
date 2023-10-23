d187023c14cfb27ba996d4afdb69c595ba47384a
Neville Mpiana <nevillempiana0619@gmail.com>
2023-10-23 16:13:16 +0200

    poe

---
+++ OPSC6311/ST10101126OPSCPart2/app/src/main/java/za/ac/iie/opsc7311/planmylife/Dashboard.kt
@@ -0,0 +1,11 @@
+package za.ac.iie.opsc7311.planmylife
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Dashboard : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard2)
+    }
+}
---
+++ OPSC6311/ST10101126OPSCPart2/app/src/main/java/za/ac/iie/opsc7311/planmylife/SignInActivity.kt
@@ -0,0 +1,11 @@
+package za.ac.iie.opsc7311.planmylife
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
